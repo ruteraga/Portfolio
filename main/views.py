ddaea2414c9cf921a0ff7af28044f2580fee5307
ruteraga <ganzagabin1@gmail.com>
2025-04-29 00:39:00 +0200

    first commit

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Project, Tag, ProjectImage
+# Create your views here.
+
+def home(request):
+    return render(request, "index.html")
+
+def project(request):
+    projects=Project.objects.prefetch_related('images','tags').all()
+    return render(request, "project.html", {"projects":projects})
